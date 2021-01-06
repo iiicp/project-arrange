ad2a2bb37899a7ce72e029573ada993adc2ea64e
caipeng <iiicp@outlook.com>
2021-01-06 18:04:33 +0800

    about openblas test

--- performance-optimazing/main.cpp
+++ performance-optimazing/main.cpp
@@ -5,19 +5,68 @@
 int main() {
 
     OpenblasTest::MyTest mytest;
-    OpenblasTest::Matrix mat(256,161);
+    
+    // Mat x Mat  C = alpha * A * B + beta * C
+    int row1 = 256, col1 = 128;
+    int row2 = 128, col2 = 64;
+    OpenblasTest::Matrix<float> mat(row1,col1);
+    OpenblasTest::Matrix<float> mat2(row2,col2);
     mat.randomize();
-    //mat.dumpMat();
+    mat2.randomize();
 
-    OpenblasTest::Vec vec(161);
+    std::cout << "C = alpha * A * B + beta * C: " << "[" << row1 << "," << col1 <<"] x " << "[" << row2 << "," << col2 << "]" << std::endl;
+    clock_t start = clock();
+    OpenblasTest::Matrix<float> m = mytest.MatMulMat(mat, mat2);
+    clock_t end = clock();
+    std::cout << "native cpp time: " << (double)(end - start) / 1000 << "ms" << std::endl;
+
+    start = clock();
+    OpenblasTest::Matrix<float> m_blas = mytest.MatMulMat_blas(mat, mat2);
+    end = clock();
+    std::cout << "openblas time: " << (double)(end - start) / 1000 << "ms" << std::endl;
+    
+    // Mat x Vec  C = alpha * A * b + beta * C;
+    row1 = 256;
+    col1 = 161;
+    OpenblasTest::Matrix<float> mat3(row1,col1);
+    mat3.randomize();
+    //mat3.dumpMat();
+    
+    row2 = 161;
+    OpenblasTest::Vec<float> vec(row2);
     vec.randomize();
     //vec.dumpVec();
-
-    clock_t start = clock();
-    OpenblasTest::Vec v = mytest.MatMulVec(mat, vec);
-    clock_t end = clock();
-    std::cout << "time: " << (double)(end - start) / 1000 << "ms" << std::endl;
-    //v.dumpVec();
-
+    
+    std::cout << "C = alpha * A * b + beta * C: " << "[" << row1 << "," << col1 <<"] x " << "[" << row2 << ", 1"<< "]" << std::endl;
+    start = clock();
+    OpenblasTest::Vec<float> c = mytest.MatMulVec(mat3, vec);
+    end = clock();
+    //c.dumpVec();
+    std::cout << "native cpp time: " << (double)(end - start) / 1000 << "ms" << std::endl;
+    
+    start = clock();
+    OpenblasTest::Vec<float> c_blas = mytest.MatMulVec_blas(mat3, vec);
+    end = clock();
+    //c_blas.dumpVec();
+    std::cout << "openblas time: " << (double)(end - start) / 1000 << "ms" << std::endl;
+    
+    // Vec + Vec  y = alpha * x + y;
+    row1 = 512;
+    OpenblasTest::Vec<float> vec3(row1);
+    vec3.randomize();
+    OpenblasTest::Vec<float> vec4(row1);
+    vec4.randomize();
+    OpenblasTest::Vec<float> vec5(vec4);
+    
+    std::cout << "y = alpha * x + y: " << "[" << row1 << ",1" <<"] + " << "[" << row1 << ",1" <<"]" << std::endl;
+    start = clock();
+    mytest.VecAddVec(vec3, vec4, 1.0f);
+    end = clock();
+    std::cout << "native cpp time: " << (double)(end - start) / 1000 << "ms" << std::endl;
+    
+    start = clock();
+    mytest.VecAddVec_blas(vec3, vec5, 1.0f);
+    end = clock();
+    std::cout << "openblas time: " << (double)(end - start) / 1000 << "ms" << std::endl;
     return 0;
 }
--- performance-optimazing/openblastest.h
+++ performance-optimazing/openblastest.h
@@ -1,146 +1,219 @@
 /***********************************
-* File:     openblastest.h
-*
-* Author:   caipeng
-*
-* Email:    iiicp@outlook.com
-*
-* Date:     2021/1/5
-***********************************/
+ * File:     openblastest.h
+ *
+ * Author:   caipeng
+ *
+ * Email:    iiicp@outlook.com
+ *
+ * Date:     2021/1/5
+ ***********************************/
 
 #ifndef PERFORMANCE_OPTIMAZING_OPENBLASTEST_H
 #define PERFORMANCE_OPTIMAZING_OPENBLASTEST_H
 
 #include <Accelerate/Accelerate.h>
-#include <time.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Moving a local object in a return statement prevents copy elision
+ *
+ * https://www.zhihu.com/question/57048704/answer/151446405
+ *
+ * C++ 类型判断
+ * std::is_same<T1, T2>
+ */
 
 namespace OpenblasTest {
-    struct Matrix {
-        int row;
-        int col;
-        float **data;
-        Matrix(int row, int col) {
-            assert(row > 0 && col > 0);
-            this->row = row;
-            this->col = col;
-            data = new float* [row];
-            for (int i = 0; i < row; ++i) {
-                data[i] = new float[col];
-            }
+template <typename T> struct Matrix {
+  int row;
+  int col;
+  T *data;
+  Matrix(int row, int col) {
+    assert(row > 0 && col > 0);
+    this->row = row;
+    this->col = col;
+    data = new T[row * col];
 
-            for (int i = 0; i < row; ++i) {
-                for (int j = 0; j < col; ++j) {
-                    data[i][j] = 0;
-                }
-            }
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        data[i * col + j] = 0;
+      }
+    }
+  }
+
+  /// move 构造函数签名是非const，因为要把内存存储区域设置为NULL
+  Matrix(Matrix &&mat) : row(mat.row), col(mat.col), data(mat.data) {
+    printf("move matrix...\n");
+    mat.data = NULL;
+  }
+
+  void randomize() {
+    srand((unsigned)time(nullptr));
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        if (std::is_same<T, int>::value)
+          data[i * col + j] = (rand() % 10);
+        else {
+          data[i * col + j] = (rand() % 10) * 1.0f;
         }
+      }
+    }
+  }
 
-        void randomize() {
-            srand(time(nullptr));
-            for (int i = 0; i < row; ++i) {
-                for (int j = 0; j < col; ++j) {
-                    data[i][j] = rand() % 10;
-                }
-            }
+  void dumpMat() {
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        std::cout << data[i * col + j] << " ";
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+
+  ~Matrix() {
+    if (data) {
+      delete[] data;
+    }
+  }
+};
+
+template <typename T> struct Vec {
+  int row;
+  T *data;
+
+  Vec(const Vec &v) {
+    std::cout << "copy construct..." << std::endl;
+    row = v.row;
+    data = new T[row];
+    for (int i = 0; i < v.row; ++i)
+      data[i] = v.data[i];
+  }
+
+  Vec(Vec &&v) : data(v.data), row(v.row) {
+    std::cout << "move construct..." << std::endl;
+    v.data = NULL;
+  }
+
+  Vec &operator=(const Vec &v) {
+    std::cout << "operator=" << std::endl;
+    row = v.row;
+    for (int i = 0; i < row; ++i)
+      data[i] = v.data[i];
+    return *this;
+  }
+
+  Vec(int row) {
+    assert(row > 0);
+    this->row = row;
+    data = new T[row];
+    for (int i = 0; i < row; ++i)
+      data[i] = 0;
+  }
+
+  void randomize() {
+    srand((unsigned)time(nullptr));
+    for (int i = 0; i < row; ++i)
+      data[i] = (rand() % 10) * 1.0f;
+  }
+
+  void dumpVec() {
+    printf("\n");
+    for (int i = 0; i < row; ++i)
+      std::cout << data[i] << " ";
+    printf("\n");
+  }
+
+  T &operator[](int i) { return data[i]; }
+
+  ~Vec() {
+    if (data) {
+      delete[] data;
+    }
+  }
+};
+class MyTest {
+public:
+  /// y = alpha * x + y;
+  void VecAddVec(Vec<float> &x, Vec<float> &y, float alpha) {
+    assert(x.row == y.row);
+    int n = x.row;
+    for (int i = 0; i < n; ++i) {
+      y[i] += x[i] * alpha;
+    }
+  }
+
+  /// y = alpha * x + y; (小规模向量会负优化，至少要大于512)
+  void VecAddVec_blas(Vec<float> &x, Vec<float> &y, float alpha) {
+    assert(x.row == y.row);
+    int n = x.row;
+    cblas_saxpy(n, alpha, x.data, 1, y.data, 1);
+  }
+
+  /// C = alpha * A * b + beta * C
+  Vec<float> MatMulVec(Matrix<float> &A, Vec<float> &b) {
+    assert(A.col == b.row);
+
+    Vec<float> c(A.row);
+
+    int M = A.row, N = A.col;
+    for (int i = 0; i < M; ++i) {
+      for (int j = 0; j < N; ++j) {
+        c[i] += A.data[i * N + j] * b[j];
+      }
+    }
+
+    return c;
+  }
+
+  /// C = alpha * A * b + beta * C
+  Vec<float> MatMulVec_blas(Matrix<float> &A, Vec<float> &b) {
+    assert(A.col == b.row);
+
+    Vec<float> c(A.row);
+
+    int M = A.row, N = A.col;
+    float alpha = 1.0f, beta = 0.0f;
+    int lda = A.col;
+    cblas_sgemv(CblasRowMajor, CblasNoTrans, M, N, alpha, A.data, lda, b.data,
+                1, beta, c.data, 1);
+
+    return c;
+  }
+
+  /// C = alpha * A * B + beta * C
+  Matrix<float> MatMulMat(Matrix<float> &A, Matrix<float> &B) {
+    assert(A.col == B.row);
+
+    Matrix<float> C(A.row, B.col);
+    int M = A.row, N = B.col, K = B.row;
+    for (int i = 0; i < M; ++i) {
+      for (int j = 0; j < N; ++j) {
+        for (int k = 0; k < K; ++k) {
+          C.data[i * N + j] += A.data[i * A.col + k] * B.data[k * N + j];
         }
+      }
+    }
+    return C;
+  }
 
-        void dumpMat() {
-            for (int i = 0; i < row; ++i) {
-                for (int j = 0; j < col; ++j) {
-                    printf("%f ", data[i][j]);
-                }
-                printf("\n");
-            }
-            printf("\n");
-        }
+  /// C = alpha * A * B + beta * C
+  Matrix<float> MatMulMat_blas(Matrix<float> &A, Matrix<float> &B) {
+    assert(A.col == B.row);
 
-        ~Matrix() {
-            if (data) {
-                for (int i = 0; i < row; ++i)
-                    delete[] data[i];
-                delete[] data;
-            }
-        }
+    Matrix<float> C(A.row, B.col);
 
-        float get(int row, int col) const{
-            return data[row][col];
-        }
-    };
+    int M = A.row, N = B.col, K = B.row;
+    float alpha = 1.0f, beta = 0.0f;
+    int lda = A.col, ldb = B.col, ldc = B.col;
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha,
+                A.data, lda, B.data, ldb, beta, C.data, ldc);
 
-    struct Vec {
-        int row;
-        float *data;
+    return C;
+  }
+};
+} // namespace OpenblasTest
 
-        Vec(const Vec& v) {
-            std::cout << "copy construct..." << std::endl;
-            row = v.row;
-            for (int i = 0; i < v.row; ++i)
-                data[i] = v.data[i];
-        }
-
-        Vec(Vec &&v) : data(v.data), row(v.row) {
-            std::cout << "move construct..." << std::endl;
-            v.data = NULL;
-        }
-
-        Vec& operator=(const Vec& v) {
-            std::cout << "operator=" << std::endl;
-            row = v.row;
-            for (int i = 0; i < row; ++i)
-                data[i] = v.data[i];
-            return *this;
-        }
-
-        Vec(int row) {
-            assert(row > 0);
-            this->row = row;
-            data = new float[row];
-            for (int i = 0; i < row; ++i)
-                data[i] = 0;
-        }
-
-        void randomize() {
-            srand(time(nullptr));
-            for (int i = 0; i < row; ++i)
-                data[i] = rand() % 10;
-        }
-
-        void dumpVec() {
-            printf("\n");
-            for (int i = 0; i < row; ++i)
-                printf("%f ", data[i]);
-            printf("\n");
-        }
-
-        float& operator[](int i) {
-            return data[i];
-        }
-
-        ~Vec() {
-            if (data) {
-                delete[] data;
-            }
-        }
-    };
-    class MyTest{
-    public:
-        Vec MatMulVec(Matrix &matrix, Vec &vec) {
-            assert(matrix.col == vec.row);
-
-            Vec v(matrix.row);
-
-            for (int i = 0; i < matrix.row; ++i) {
-                for (int j = 0; j < matrix.col; ++j) {
-                    v[i] += matrix.get(i,j) * vec[j];
-                }
-            }
-
-            return std::move(v);
-        }
-    };
-}
-
-#endif //PERFORMANCE_OPTIMAZING_OPENBLASTEST_H
+#endif // PERFORMANCE_OPTIMAZING_OPENBLASTEST_H
